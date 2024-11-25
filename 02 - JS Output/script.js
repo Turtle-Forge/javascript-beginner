f61cac15263e826fbb1e803e13206fd87fa944fe
Benony Gabriel <gbenn.work@gmail.com>
2024-11-25 23:48:58 +0700

    belajar js output

---
+++ 02 - JS Output/script.js
@@ -0,0 +1,31 @@
+// Unutk ID demo1
+document.getElementById("demo1").innerHTML = 10 * 5;
+
+// Unutk ID demo2
+document.getElementById("demo2").innerHTML =
+  "<b>Saya</b> ditulis oleh JavaScript";
+
+// Unutk ID demo3
+var nama = "John Doe";
+function showName() {
+  document.getElementById("demo3").innerHTML = nama;
+}
+
+// Unutk ID demo4
+function writeDoc() {
+  document.write("Hello guys");
+}
+
+// untuk contoh alert
+function showAlert1() {
+  window.alert("Hello World");
+}
+
+function showAlert2() {
+  alert(10 * 2);
+}
+
+// use console.log()
+function useConsole() {
+  console.log("Oke, saya diklik");
+}
