1ac9c13685d44db43f8c7ced916c982b1bd0a257
Benony Gabriel <gbenn.work@gmail.com>
2024-11-26 09:01:01 +0700

    belajar JS statement

---
+++ 03 - JS statement/script.js
@@ -0,0 +1,31 @@
+function showAlert() {
+  alert("Hello World");
+}
+
+// JS Statement
+let x, y, res; // Statement 1
+x = 50; // Statement 2
+y = 10; // Statement 3
+res = x + y; // Statement 4
+
+// console.log(res);
+
+function changeColor() {
+  var target = document.getElementById("demo1");
+  target.style.color = "red";
+}
+
+function merah() {
+  var target = document.getElementById("merah");
+  target.style.backgroundColor = "red";
+}
+
+function putih() {
+  var target = document.getElementById("putih");
+  target.style.backgroundColor = "white";
+}
+
+function biru() {
+  var target = document.getElementById("biru");
+  target.style.backgroundColor = "blue";
+}
