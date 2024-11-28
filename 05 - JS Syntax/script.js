c3f8d46e92bdce4846b00e260e9fa71735a9193e
Benony Gabriel <gbenn.work@gmail.com>
2024-11-28 15:43:19 +0700

    Add JavaScript variable creation and usage examples

---
+++ 05 - JS Syntax/script.js
@@ -0,0 +1,25 @@
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 10;
+y = 5;
+let z = x + y;
+
+// Print variables:
+console.log("x = ", x);
+console.log("y = ", y);
+console.log("z = ", z);
+
+console.log();
+// Tukar value
+var a = 5;
+var b = 3;
+console.log("a = ", a);
+console.log("b = ", b);
+
+var temp = a;
+
+console.log("a = ", a);
+console.log("b = ", b);
